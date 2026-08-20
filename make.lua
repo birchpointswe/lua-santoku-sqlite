405cadb5f387c8592ecae71d70a1f9b3a9cf15da
broma0 <121064585+broma0@users.noreply.github.com>
2026-08-20 19:08:48 -0400

    make.lua: add configure
    make.lua
      + configure

--- make.lua
+++ make.lua
@@ -1,15 +1,27 @@
+local fs = require("santoku.fs")
+local vendor = require("santoku.make.vendor")
+
+local vendored = {
+  {
+    file = "deps/sqlite3/sqlite-amalgamation-3490200.zip",
+    url = "https://www.sqlite.org/2025/sqlite-amalgamation-3490200.zip",
+    sha256 = "921fc725517a694df7df38a2a3dfede6684024b5788d9de464187c612afb5918",
+  },
+}
+
+local include = {}
+for i = 1, #vendored do
+  include[i] = vendored[i].file
+end
+
 local env = {
   name = "santoku-sqlite",
   version = "2.1.0-1",
   variable_prefix = "TK_SQLITE",
   license = "MIT",
   public = true,
-  vendor = {
-    {
-      file = "deps/sqlite3/sqlite-amalgamation-3490200.zip",
-      url = "https://www.sqlite.org/2025/sqlite-amalgamation-3490200.zip",
-      sha256 = "921fc725517a694df7df38a2a3dfede6684024b5788d9de464187c612afb5918",
-    },
+  rules = {
+    include = include,
   },
   cflags = {
     "-I$(PWD)/deps/sqlite3/",
@@ -29,6 +41,15 @@ local env = {
       "santoku-matrix >= 2.0.0, < 3.0.0",
     },
   },
+  configure = function (submake, envs)
+    for i = 1, #vendored do
+      local v = vendored[i]
+      local dest = fs.join(envs.root.build_dir, v.file)
+      submake.target({ dest }, { "make.lua" }, function ()
+        vendor.fetch(v, dest)
+      end)
+    end
+  end,
 }
 
 env.homepage = "https://github.com/birchpointswe/lua-" .. env.name
